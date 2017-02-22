220cd1e370e4b593df733d087668dcaa1cec9527
Cuttlefish <zh4n@live.co.uk>
2017-02-22 18:41:16 +0000

    Movement and Colour
    Oscillation of movement and colour using trigonometry

---
+++ movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	float r,g,b = 1.0f;
+
+	void FixedUpdate () {
+
+		//colour oscillation
+		r = Mathf.Sin (Time.time * 1f);
+		g = Mathf.Sin (Time.time * 2f);
+		b = Mathf.Sin (Time.time * 3f);
+		GetComponent<Renderer> ().material.color = new Color (r, g, b, 1f);
+
+		//movement oscillation
+		transform.Translate (
+			(Mathf.Sin(Time.time*3f)/20),
+			(Mathf.Cos(Time.time*3f)/40),
+			(Mathf.Cos(Time.time*3f)/30)
+		);
+	}
+}
